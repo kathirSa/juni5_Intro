3e8f512e842cacfa1fa12b83574182feca562ddc
sankat_gmx <sanje.ka@gmx.de>
2021-06-05 19:00:24 +0200

    Lamda to aseert fail text

--- src/test/java/MathUtilTestCase.java
+++ src/test/java/MathUtilTestCase.java
@@ -1,18 +1,22 @@
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_METHOD)
+@DisplayName("Mathe Tests")
 public class MathUtilTestCase {
 
     MathUtil mathUtil;
+    TestInfo testInfo;
+    TestReporter testReporter;
 
     @BeforeEach
-    void init(){
+    void init(TestInfo testInfo, TestReporter testReporter){
+        this.testInfo = testInfo;
+        this.testReporter = testReporter;
         mathUtil = new MathUtil();
+        testReporter.publishEntry("Test case -> " + testInfo.getDisplayName());
+
     }
 
     @Test
@@ -21,7 +25,7 @@ public class MathUtilTestCase {
         Box<Integer> exp = new Box<>(6);
         Box<Integer> act = new Box<>(mathUtil.addTwoNumbers(2,4));
 
-        assertEquals(exp.getValue(),act.getValue(),"Add two int numbers, should return the correct  sum");
+        assertEquals(exp.getValue(),act.getValue(),() -> "Add two int numbers, should return the correct  sum");
     }
 
     @Test
@@ -30,7 +34,7 @@ public class MathUtilTestCase {
         Box<Double> exp = new Box<>(314.1592653589793);
         Box<Double> act = new Box<>(mathUtil.circleArea(10));
 
-        assertEquals(exp.getValue(),act.getValue(),"Given is a circle radius, should calculsate the area of a circle");
+        assertEquals(exp.getValue(),act.getValue(),() -> "Given is a circle radius, should calculsate the area of a circle");
     }
 
 }
