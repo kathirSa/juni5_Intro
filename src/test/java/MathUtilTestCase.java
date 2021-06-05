b84f1657c4d91de2e0626217cc5f212bb6a3e825
piradab <krishna>
2021-06-05 15:01:34 +0200

    test

--- src/test/java/MathUtilTestCase.java
+++ src/test/java/MathUtilTestCase.java
@@ -1,30 +1,32 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
 public class MathUtilTestCase {
+
+    MathUtil mathUtil;
+
+    @BeforeEach
+    void init(){
+        mathUtil = new MathUtil();
+    }
 
     @Test
     @DisplayName("Add two int numbers, should return the correct  sum")
      public void addTwoNumberstest() {
-
-        MathUtil mathUtil = new MathUtil();
-
         Box<Integer> exp = new Box<>(6);
         Box<Integer> act = new Box<>(mathUtil.addTwoNumbers(2,4));
 
         assertEquals(exp.getValue(),act.getValue(),"Add two int numbers, should return the correct  sum");
     }
 
-
-
     @Test
     @DisplayName("Given is a circle radius, should calculate the area of a circle")
     public void circleAreatest() {
-
-        MathUtil mathUtil = new MathUtil();
-
         Box<Double> exp = new Box<>(314.1592653589793);
         Box<Double> act = new Box<>(mathUtil.circleArea(10));
 
